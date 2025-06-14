1d758ade8d69ba4bfa364b5150441bd41292188b
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2025-06-14 11:32:54 +0200

    Change api of monitor

--- cmd/main.go
+++ cmd/main.go
@@ -7,20 +7,87 @@ import (
 	"github.com/mati2251/distributed-monitor/pkg/rem"
 )
 
-func main() {
-	config, error := rem.ReadConfig("config.json")
-	if error != nil {
-		fmt.Printf("failed to read config: %s\n", error.Error())
+type ReaderWriterData struct {
+	n   int
+	c   int
+	ar  int
+	aw  int
+	buf []int
+}
 
+func writer(c *rem.Cond, rw *ReaderWriterData) error {
+	for rw.n != 0 {
+		c.Wait()
 	}
-	socket, err := rem.NewSocket(*config)
+	rw.n = rw.n + rw.aw
+	for range 5 {
+		rw.buf = append(rw.buf, rw.c)
+		rw.c++
+		fmt.Println("Writer, value:", rw.buf[len(rw.buf)-1])
+	}
+	time.Sleep(100 * time.Millisecond)
+	rw.n = rw.n - rw.aw
+	c.SignalAll()
+	return nil
+}
+
+func reader(c *rem.Cond, rw *ReaderWriterData) error {
+	for rw.n&rw.aw == rw.aw || len(rw.buf) == 0 {
+		c.Wait()
+	}
+	rw.n = rw.n + rw.ar
+	fmt.Println("Reader, value:", rw.buf[0])
+	rw.buf = rw.buf[1:]
+	time.Sleep(100 * time.Millisecond)
+	rw.n = rw.n - rw.ar
+	c.SignalAll()
+	return nil
+}
+
+type ReaderWriter struct {
+	monitor *rem.Monitor[*ReaderWriterData]
+}
+
+func (rw *ReaderWriter) Reader() {
+	rw.monitor.Synchronized(reader)
+}
+
+func (rw *ReaderWriter) Writer() {
+	rw.monitor.Synchronized(writer)
+}
+
+func NewReaderWriter() (*ReaderWriter, error) {
+	monitor, err := rem.NewMonitor(&ReaderWriterData{
+		n:   0,
+		c:   0,
+		ar:  2,
+		aw:  1,
+		buf: make([]int, 0),
+	})
 	if err != nil {
-		fmt.Printf("failed to create socket: %s\n", err.Error())
+		fmt.Printf("failed to create monitor: %s\n", err.Error())
+		return nil, err
+	}
+	rw := &ReaderWriter{
+		monitor: monitor,
+	}
+	return rw, nil
+}
+
+func main() {
+	rw, err := NewReaderWriter()
+	if err != nil {
+		fmt.Printf("failed to create ReaderWriter: %s\n", err.Error())
 		return
 	}
-	defer socket.Close()
-	fmt.Println("Socket created successfully")
-	time.Sleep(1 * time.Second)
-	socket.Pub.Send("1 eloo", 0)
-	socket.Loop()
+
+	for range 25 {
+		go rw.Reader()
+	}
+
+	for range 5 {
+		go rw.Writer()
+	}
+
+	time.Sleep(10 * time.Second)
 }
--- pkg/rem/monitor.go
+++ pkg/rem/monitor.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 	"sync"
-
-	zmq "github.com/pebbe/zmq4"
+	// zmq "github.com/pebbe/zmq4"
 )
 
 type PeerConfig struct {
@@ -40,162 +38,58 @@ func ReadConfig(path string) (*Config, error) {
 	return &config, nil
 }
 
-type Monitor struct {
-	cond         Conditinal
-	tokenOwner   bool
-	lockAcquired bool
-	requester    bool
-	mutex        sync.Mutex
-	sub         *zmq.Socket
-	Pub         *zmq.Socket
-	zctx        *zmq.Context
-	config      *Config
-	requesteres []string
-	message     chan string
+type Context struct {
+	config Config
 }
 
-func NewMonitor(config Config) (*Monitor, error) {
-	s := &Monitor{}
-	s.config = &config
-	var err error
-	s.zctx, err = zmq.NewContext()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create zmq context: %w", err)
-	}
-
-	s.sub, err = s.zctx.NewSocket(zmq.SUB)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create zmq socket: %w", err)
-	}
-	selfHost := ""
-	for _, peer := range config.Peers {
-		if peer.Id == config.Self {
-			selfHost = peer.Host
-			continue
-		}
-		err = s.sub.Connect(peer.Host)
-		if err != nil {
-			return nil, fmt.Errorf("failed to connect zmq subscriber socket to peer %s: %w", peer.Host, err)
-		}
-	}
-	err = s.sub.SetSubscribe(fmt.Sprintf("%d", config.Self))
-	if err != nil {
-		return nil, fmt.Errorf("failed to set zmq subscription for self: %w", err)
-	}
-	if config.Self == config.Token {
-		err := s.sub.SetSubscribe("token")
-		if err != nil {
-			return nil, fmt.Errorf("failed to set zmq subscription for token: %w", err)
-		}
-	}
-
-	s.Pub, err = s.zctx.NewSocket(zmq.PUB)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create zmq publisher socket: %w", err)
-	}
-	err = s.Pub.Bind(selfHost)
-	if err != nil {
-		return nil, fmt.Errorf("failed to bind zmq publisher socket: %w", err)
-	}
-	return s, nil
+type Cond struct {
+	cond *sync.Cond
 }
 
-func (s *Monitor) Close() {
-	s.Pub.Close()
-	s.sub.Close()
+func NewCond() (*Cond, error) {
+	c := &Cond{
+		cond: sync.NewCond(&sync.Mutex{}),
+	}
+	return c, nil
 }
 
-func (s *Monitor) Loop() {
-	for {
-		msg, err := s.sub.RecvMessage(0)
-		if err != nil {
-			fmt.Printf("failed to receive zmq message: %s\n", err.Error())
-			continue
-		}
-		if len(msg) == 0 {
-			continue
-		}
-		switch msg[0] {
-		case fmt.Sprintf("%d", s.config.Self):
-			fmt.Printf("Received message for self: %s\n", msg[1])
-			s.message <- msg[1]
-		case "token":
-			fmt.Printf("Received token message: %s\n", msg[1])
-			s.requesteres = append(s.requesteres, msg[1])
-		default:
-			fmt.Printf("Received message from peer: %s\n", msg[0])
-		}
-	}
+func (c *Cond) lock() {
+	c.cond.L.Lock()
 }
 
-type Synchronized interface {
-	Execute(cond Conditinal) error
+func (c *Cond) unlock() {
+	c.cond.L.Unlock()
 }
 
-type Conditinal interface {
-	Wait() error
-	Signal() error
-	SignalAll() error
+func (c *Cond) Wait() {
+	c.cond.Wait()
 }
 
-func (m *Monitor) Execute(sync Synchronized) error {
-	if sync == nil {
-		return nil
-	}
-	if err := sync.Execute(m.cond); err != nil {
-		return err
-	}
-	return nil
+func (c *Cond) SignalAll() {
+	c.cond.Broadcast()
 }
 
-func (m *Monitor) HandleToken(who string) error {	
-	m.mutex.Lock()
-	if !m.lockAcquired {	
-		m.tokenOwner = false
-		m.mutex.Unlock()
-		_, err := m.Pub.Send(fmt.Sprintf("%d token %d", who, m.config.Self), 0)
-		if err != nil {
-			return fmt.Errorf("failed to send token request: %w", err)
-		}
-		msg := <- m.message
-		if msg != "ok" {
-			return fmt.Errorf("unexpected message received: %s", msg)
-		}
-		m.sub.SetUnsubscribe("token")
-	}
-	// m.requesteres = append(m.requesteres, msg)
+func (c *Cond) Signal() {
+	c.cond.Signal()
 }
 
-func (m *Monitor) Lock() error {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-	if m.lockAcquired {
-		return fmt.Errorf("lock already acquired")
+type Monitor[T any] struct {
+	c   *Cond
+	val T
+}
+
+func NewMonitor[T any](val T) (*Monitor[T], error) {
+	m := &Monitor[T]{
+		c:   &Cond{cond: sync.NewCond(&sync.Mutex{})},
+		val: val,
 	}
-	if m.tokenOwner {
-		m.lockAcquired = true
-		return nil
-	}
-	m.mutex.Unlock()
-	_, err := m.Pub.Send(fmt.Sprintf("token %d", m.config.Self), 0)
-	if err != nil {
-		return fmt.Errorf("failed to send token request: %w", err)
-	}
-	mes := <-m.message
-	mesSplit := strings.Split(mes, " ")
-	if len(mesSplit) < 2 {
-		return fmt.Errorf("invalid message format received: %s", mes)
-	}
-	if mesSplit[0] != "token" {
-		return fmt.Errorf("unexpected message received: %s", mes)
-	}
-	m.mutex.Lock()
-	m.tokenOwner = true
-	m.mutex.Unlock()
-	m.sub.SetSubscribe("token")
-	m.Pub.Send(fmt.Sprintf("%s ok", mesSplit[1]), 0)
-	m.mutex.Lock()
-	m.lockAcquired = true
-	m.mutex.Unlock()
-	return nil
+	return m, nil
+}
+
+type SynchronizedFunc[T any] func(m *Cond, val T) error
+
+func (m Monitor[T]) Synchronized(f SynchronizedFunc[T]) error {
+	m.c.lock()
+	defer m.c.unlock()
+	return f(m.c, m.val)
 }
